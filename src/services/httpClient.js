1beeb63a312aa2d1a6f065a65c01639efae770ab
Matekuor <51318778+Tekuor@users.noreply.github.com>
2021-06-13 09:09:52 +0000

    changed base url

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: "https://unsplash-api-g.herokuapp.com/api/",
     timeout: 90000,
     headers: {
         "Content-Type": "application/json",
